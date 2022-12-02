f9957643b30158445f35d6164b51167190b9cc67
Alexander Sinyagin <Sin.D.Alex99@gmail.com>
2022-12-02 13:44:17 +0500

    modify

--- src/LoggingServiceProvider.php
+++ src/LoggingServiceProvider.php
@@ -13,8 +13,6 @@ class LoggingServiceProvider extends ServiceProvider
 {
   public function boot()
   {
-
-
     $this->publishes([
       __DIR__ . '/config/config.php' => config_path('logging.php'),
     ]);
@@ -31,8 +29,6 @@ class LoggingServiceProvider extends ServiceProvider
 
   public function register()
   {
-    $this->app->register('Tridmedia\Logging\LoggingServiceProvider');
-
     $this->mergeConfigFrom(__DIR__ . '/config/config.php', 'logging');
   }
 }
